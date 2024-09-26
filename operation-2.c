e454523e06900e42af77e6a7e9330f56c39a1520
Amiir25 <amirsadik636@gmail.com>
2024-09-26 15:37:36 +0300

    Update operation-2.c

--- operation-2.c
+++ operation-2.c
@@ -32,3 +32,54 @@ void pop(stack_t **head, int line_number)
 		free(ptr);
 	}
 }
+
+/**
+ * swap - Swaps the top two elements of the stack
+ * @head: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void swap(stack_t **head, int line_number)
+{
+	stack_t *ptr;
+	stack_t *node1;
+	stack_t *node2;
+	int count;
+
+	ptr = node1 = node2 = *head;
+	count = 1;
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	node2 = node2->next;
+	if (node2->next != NULL)
+	{
+		node1->next = node2->next;
+		node1->prev = node2;
+		node2->next->prev = node1;
+		node2->next = node1;
+		node2->prev = NULL;
+	}
+
+	else
+	{
+		node1->next = NULL;
+		node1->prev = node2;
+		node2->next = node1;
+		node2->prev = NULL;
+	}
+
+	*head = node2;
+}
