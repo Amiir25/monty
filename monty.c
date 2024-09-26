86f2a184f32a6581c73128663910ef103ac2291f
Amiir25 <amirsadik636@gmail.com>
2024-09-26 14:07:02 +0300

    Update monty.c

--- monty.c
+++ monty.c
@@ -82,19 +82,17 @@ void get_opcode(stack_t **head, char *line, int line_number)
 	opcode = strtok(line, " \n\t\r");		/* Extract the first word */
 	if (opcode == NULL || opcode[0] == '#')		/* Check if it is empty or comment */
 		return;
-
 	if (strcmp(opcode, "push") == 0)
 	{
 		arg = strtok(NULL, " \n\t\r");		/* Extract the arguement of push */
 		push(head, line_number, arg);
 	}
-
 	else if (strcmp(opcode, "pall") == 0)
 		pall(head);
-
 	else if (strcmp(opcode, "pint") == 0)
 		pint(head, line_number);
-
+	else if (strcmp(opcode, "pop") == 0)
+		pop(head, line_number);
 	else	/* Unkown opcode error */
 	{
 		fprintf(stderr, "L%d: unkown instruction %s\n", line_number, opcode);
