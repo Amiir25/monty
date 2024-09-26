34c9d76df64ee9068e58260fc2509018f5f27f6b
Amiir25 <amirsadik636@gmail.com>
2024-09-26 14:09:49 +0300

    Update operation-2.c

---
+++ operation-2.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * pop - Remove the top element of the stack
+ * @head: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void pop(stack_t **head, int line_number)
+{
+	stack_t *ptr;
+
+	if (is_empty(head))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	ptr = *head;
+	if (ptr->next != NULL)
+	{
+		*head = ptr->next;
+		(*head)->prev = NULL;
+	}
+
+	else
+	{
+		*head = NULL;
+		free(ptr);
+	}
+}
