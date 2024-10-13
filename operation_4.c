bff58c451091417c98e84244aef1b4c8d338e1d4
Amiir25 <amirsadik636@gmail.com>
2024-10-13 16:31:08 +0300

    Update operation_4.c

--- operation_4.c
+++ operation_4.c
@@ -2,6 +2,69 @@
 
 
 /**
+ * pchar - Prints the a character from its ascii equivalent
+ * @stack: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int asci;
+	char to_char;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	asci = (*stack)->n;
+	if (asci < 33 || asci > 126)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	to_char = (char)asci;
+	printf("%c\n", to_char);
+}
+
+/**
+ * pstr - Prints the a string from its ascii equivalent
+ * @stack: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr;
+	char to_char;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	ptr = *stack;
+	while (ptr)
+	{
+		if (ptr->n < 33 || ptr->n > 126)
+			break;
+
+		to_char = (char)ptr->n;
+		ptr = ptr->next;
+		printf("%c", to_char);
+	}
+
+	printf("\n");
+}
+
+/**
  * rot1 - Prints the a string from its ascii equivalent
  * @stack: The address of the pointer to the first node
  * @line_number: The line number in the file
