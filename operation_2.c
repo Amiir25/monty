c09d940a2aec407dad329c49aa7ca14b5209c11f
Amiir25 <amirsadik636@gmail.com>
2024-10-13 14:39:20 +0300

    Update operation_2.c

--- operation_2.c
+++ operation_2.c
@@ -128,12 +128,7 @@ void add(stack_t **stack, unsigned int line_number)
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
-
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+	(void)stack;
 }
 
 /**
