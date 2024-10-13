e8db5d8bed8d8933c9e56cb3299a1a938137af0f
Amiir25 <amirsadik636@gmail.com>
2024-10-13 14:51:41 +0300

    Update operation_2.c

--- operation_2.c
+++ operation_2.c
@@ -142,29 +142,18 @@ void nop(stack_t **stack, unsigned int line_number)
 void sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *ptr;
-	stack_t *node1;
-	stack_t *node2;
-	int count;
+	stack_t *top;
 
-	ptr = *stack;
-	count = 0;
-
-	while (ptr != NULL)
+	if (*stack == NULL || *stack->next == NULL)
 	{
-		count++;
-		ptr = ptr->next;
-	}
-
-	if (count < 2)
-	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	node1 = *stack;
-	node2 = node1->next;
-	node2->n = node2->n - node1->n;
-	node2->prev = NULL;
-	*stack = node2;
-	free(node1);
+	top = *stack;
+	top->next->n -= top->n;
+
+	*stack = top->next;
+	(*stack)->prev = NULL;
+	free(top);
 }
