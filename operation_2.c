6d7daca8b74089725e5ef70454877a257e291164
Amiir25 <amirsadik636@gmail.com>
2024-10-13 14:36:06 +0300

    Update operation_2.c

--- operation_2.c
+++ operation_2.c
@@ -118,6 +118,25 @@ void add(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * nop - Doesn't do anything
+ * @stack: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
  * sub - Subtracts the value of the first top node from the second one
  * @stack: The address of the pointer to the first node
  * @line_number: The line number in the file
@@ -150,50 +169,6 @@ void sub(stack_t **stack, unsigned int line_number)
 	node1 = *stack;
 	node2 = node1->next;
 	node2->n = node2->n - node1->n;
-	node2->prev = NULL;
-	*stack = node2;
-	free(node1);
-}
-
-/**
- * _div - Divides the second top value by the top value of the stack
- * @stack: The address of the pointer to the first node
- * @line_number: The line number in the file
- *
- * Return: Nothing
- */
-
-void _div(stack_t **stack, unsigned int line_number)
-{
-	stack_t *ptr;
-	stack_t *node1;
-	stack_t *node2;
-	int count;
-
-	ptr = *stack;
-	count = 0;
-
-	while (ptr != NULL)
-	{
-		count++;
-		ptr = ptr->next;
-	}
-
-	if (count < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	node1 = *stack;
-	node2 = node1->next;
-	if (node2->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	node2->n = node2->n / node1->n;
 	node2->prev = NULL;
 	*stack = node2;
 	free(node1);
