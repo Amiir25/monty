72b52a9bd21c80f7dca4f7bfe0650bb45d9788c9
Amiir25 <amirsadik636@gmail.com>
2024-10-20 21:25:02 +0300

    Update operation_2.c

--- operation_2.c
+++ operation_2.c
@@ -1,6 +1,24 @@
 #include "monty.h"
 
 /**
+ * pint - Prints the value at the top of the stack
+ * @stack: The address of the pointer to the first node
+ * @line_number: The line number in the file
+ *
+ * Return: Nothing
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
  * pop - Remove the top element of the stack
  * @stack: The address of the pointer to the first node
  * @line_number: The line number in the file
@@ -117,30 +135,4 @@ void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 	(void)stack;
-}
-
-/**
- * sub - Subtracts the value of the first top node from the second one
- * @stack: The address of the pointer to the first node
- * @line_number: The line number in the file
- *
- * Return: Nothing
- */
-
-void sub(stack_t **stack, unsigned int line_number)
-{
-	stack_t *top;
-
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	top = *stack;
-	top->next->n -= top->n;
-
-	*stack = top->next;
-	(*stack)->prev = NULL;
-	free(top);
 }
